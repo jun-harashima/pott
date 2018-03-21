50c7ead0179ed6f311d42cbf9fcfefc710fbd8a2
takahi-i <takahikoito@gmail.com>
2018-03-21 18:00:35 +0900

    Fix cli test case

--- paper/cli.py
+++ paper/cli.py
@@ -6,26 +6,22 @@ from paper.librarian import Librarian
 
 
 @click.group()
-def cmd():
+def main():
     pass
 
 
-@cmd.command()
+@main.command()
 @click.argument('keywords', nargs=-1)
 def search(keywords):
     librarian = Librarian()
     papers = librarian.search(keywords)
     for index, paper in enumerate(papers):
-        print(str(index) + '. ' + paper['title'], end='')
+        print(str(index) + '. ' + paper['title'])
         print(' (PDF is N/A)' if paper['url'] is None else '')
         print('   ' + ', '.join(paper['authors']))
     user_input = librarian.get_user_input(papers)
     librarian.save(papers[user_input])
     return 0
-
-
-def main():
-    cmd()
 
 
 if __name__ == "__main__":
--- tests/test_paper.py
+++ tests/test_paper.py
@@ -5,30 +5,20 @@
 
 
 import unittest
-# from click.testing import CliRunner
+from click.testing import CliRunner
 
-# from paper import paper
-# from paper import cli
+from paper import cli
 
 
 class TestPaper(unittest.TestCase):
     """Tests for `paper` package."""
 
-    # def setUp(self):
-    #     """Set up test fixtures, if any."""
-
-    # def tearDown(self):
-    #     """Tear down test fixtures, if any."""
-
-    # def test_000_something(self):
-    #     """Test something."""
-
-    # def test_command_line_interface(self):
-    #     """Test the CLI."""
-    #     runner = CliRunner()
-    #     result = runner.invoke(cli.main)
-    #     assert result.exit_code == 0
-    #     assert 'paper.cli.main' in result.output
-    #     help_result = runner.invoke(cli.main, ['--help'])
-    #     assert help_result.exit_code == 0
-    #     assert '--help  Show this message and exit.' in help_result.output
+    def test_command_line_interface(self):
+        """Test the CLI."""
+        runner = CliRunner()
+        result = runner.invoke(cli.main)
+        assert result.exit_code == 0
+        assert 'main' in result.output
+        help_result = runner.invoke(cli.main, ['--help'])
+        assert help_result.exit_code == 0
+        assert '--help  Show this message and exit.' in help_result.output
