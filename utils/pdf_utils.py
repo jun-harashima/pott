0b78f87f861d003538e1466918c36631822967d3
Jun Harashima <j.harashima@gmail.com>
2018-04-05 13:42:20 +0900

    Save text

---
+++ utils/pdf_utils.py
@@ -0,0 +1,15 @@
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+
+def extract_text_from(pdf_file):
+    manager = PDFResourceManager()
+    stringio = StringIO()
+    laparams = LAParams()
+    device = TextConverter(manager, stringio, codec='utf-8', laparams=laparams)
+    interpreter = PDFPageInterpreter(manager, device)
+    for page in PDFPage.get_pages(pdf_file):
+        interpreter.process_page(page)
+    return stringio.getvalue()
