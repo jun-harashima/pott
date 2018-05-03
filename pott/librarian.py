a3002b7456acb8ff01ef8b1e213e412d6d66e0de
Jun Harashima <jun-harashima@cookpad.com>
2018-05-03 11:32:58 +0900

    add pdf.pf

--- pott/librarian.py
+++ pott/librarian.py
@@ -4,7 +4,7 @@ import os
 import requests
 from pyquery import PyQuery
 from pott.utils.html_utils import extract_papers_from
-from pott.utils.pdf_utils import extract_text_from
+from pott.utils.pdf import Pdf
 from pott.utils.yaml import Yaml
 from pott.utils.paper_index import PaperIndex
 
@@ -12,13 +12,9 @@ from pott.utils.paper_index import PaperIndex
 class Librarian:
 
     __SCHOLAR_URL = "https://scholar.google.com/scholar"
-    __PDF_DIR = os.environ['HOME'] + '/.pott/pdf'
     __TXT_DIR = os.environ['HOME'] + '/.pott/txt'
 
     def __init__(self):
-        if not os.path.isdir(self.__PDF_DIR):
-            os.makedirs(self.__PDF_DIR)
-
         if not os.path.isdir(self.__TXT_DIR):
             os.makedirs(self.__TXT_DIR)
 
@@ -38,16 +34,13 @@ class Librarian:
         if response.status_code != 200:
             return
 
-        pdf_name = paper['id'] + '.pdf'
         txt_name = paper['id'] + '.txt'
 
-        with open(self.__PDF_DIR + '/' + pdf_name, 'wb') as pdf_file:
-            pdf_file.write(response.content)
-
-        with open(self.__PDF_DIR + '/' + pdf_name, 'rb') as pdf_file:
-            text = extract_text_from(pdf_file)
-            with open(self.__TXT_DIR + '/' + txt_name, 'w') as txt_file:
-                txt_file.write(text)
+        pdf = Pdf(paper['id'] + '.pdf')
+        pdf.save(response.content)
+        text = pdf.extract_text()
+        with open(self.__TXT_DIR + '/' + txt_name, 'w') as txt_file:
+            txt_file.write(text)
 
         self.index.save(paper, paper['id'], txt_name)
         self.yaml.update(paper)
