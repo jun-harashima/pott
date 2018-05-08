d1a939e52cbe1e09834df9fdf9dde2522d705191
Jun Harashima <jun-harashima@cookpad.com>
2018-05-08 20:30:30 +0900

    add output_utils.py

--- pott/paper.py
+++ pott/paper.py
@@ -1,10 +1,10 @@
 class Paper:
 
-    def __init__(self, url='', title='', authors=[], year=''):
+    def __init__(self, url=None, title=None, authors=[], year=None):
         self.url = url
         self.title = title
         self.authors = authors
         self.year = year
         self.id = ''
-        if authors != [] and year != 0:
+        if authors != [] and year is not None:
             self.id = authors[0].split(' ')[-1] + year
