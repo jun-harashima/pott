02d749c31ce8f195e2593cefb6b68449210c01e8
Jun Harashima <jun-harashima@cookpad.com>
2018-05-24 09:25:45 +0900

    Also set file_path in File class

--- pott/files/file.py
+++ pott/files/file.py
@@ -3,10 +3,11 @@ import os
 
 class File:
 
-    def set_file_name(self, file_name):
+    def set_file_name_and_path(self, file_name):
         if not os.path.isdir(self.dir_name()):
             os.makedirs(self.dir_name())
         self.file_name = file_name
+        self.file_path = self.dir_name() + '/' + file_name
 
     def dir_name(self):
         raise RuntimeError("dir_name should be implemented in child classes!")
--- pott/files/pdf.py
+++ pott/files/pdf.py
@@ -12,7 +12,7 @@ class Pdf(File):
     DIR_NAME = os.environ['HOME'] + '/.pott/pdf'
 
     def __init__(self, file_name):
-        self.set_file_name(file_name)
+        self.set_file_name_and_path(file_name)
 
     def dir_name(self):
         return self.DIR_NAME
